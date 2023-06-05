c8859e7850a86c5bc3683f5320de4f16c82a7890
Jennifer Giaccai <>
2023-06-05 12:04:49 -0400

    updated to work with 2023 data, add separator info in case of comma use.

--- NMNHRamanLookSee.py
+++ NMNHRamanLookSee.py
@@ -1,56 +1,31 @@
 # Imports .txt file from NMNH Raman spectrometer, single spectrum files!
 # Loops to graph all spectra in folder
 
-import sys
-import numpy as np
-from pylab import *
-from scipy.optimize import minimize
 import matplotlib.pyplot as plt
-from scipy import signal
-from scipy.interpolate import interp1d
 import os
 import fnmatch
-from pandas import *
 import pandas as pd
 
 
 for file in os.listdir('.'):
     if fnmatch.fnmatch(file, '*.txt'):
         fullFilename = file
-        Loadfile = file[:-4]
-
-        # Data fitting parameters
-        bkd_bounds = [950, 1100, 1750, 1900] #800-1000, 1800-2000#low wavelength limits (low, high) and high wavelength limits (low, high)
-        G_bounds = [1600, 100, 60, 30] # G Peak: Center wavelength, wavelength limits, HWHM guess, HWHM limits
-        D_bounds = [1350, 100, 100, 50]  # D Peak: Center wavelength, wavelength limits, HWHM guess, HWHM limits
-        step_size = 1
-        iterations = 100
-        
-        lowG = G_bounds[0] - (G_bounds[1]/2)
-        highG = G_bounds[0] + (G_bounds[1]/2)
-        lowD = D_bounds[0] - (D_bounds[1]/2)
-        highD = D_bounds[0] + (D_bounds[1]/2)
-        
-        lowD = 1100
-        
-        
+        Loadfile = file[:-4]        
         
         # Loads the Data files
-        wave = np.loadtxt(Loadfile + '.txt', usecols = (0,), skiprows = (38))
-        signal = np.loadtxt(Loadfile + '.txt', usecols = (1,), skiprows = (38))
+        all_data = pd.read_csv(fullFilename, skiprows = (46), sep = None, header = None, engine='python', encoding='latin-1', decimal='.')
         
-        #cut-down region wave and signal to region of interest
-        wave_fit = wave[(bkd_bounds[0]-1 <= wave) & (wave <= bkd_bounds[3])]
-        signal_fit = signal[(bkd_bounds[0]-1 <= wave) & (wave <= bkd_bounds[3])]
-        
+        wave = all_data[0]
+        signal = all_data[1]
+                
         # Plotting
         fig = plt.figure(1)
         ax = fig.add_subplot(111)
-        ax.plot(wave_fit , signal_fit,'.k')
-        ax.set_xlabel('Raman Shift (cm-1)')
+        ax.plot(wave, signal,'.k')
+        ax.set_xlabel('Raman Shift / cm$^{-1}$')
         ax.set_ylabel('Raman Intensity')
         #ax.set_ylim(0, 1.5*max(signal_fit))
-        ax.legend(loc='best')
+        #ax.legend(loc='best')
         
         plt.savefig(Loadfile + '_LookSee.jpg')
         plt.close()
